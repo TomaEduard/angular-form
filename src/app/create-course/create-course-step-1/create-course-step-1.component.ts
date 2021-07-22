9063cac785ec8259ed3de7a75473a79e2bb7bb6d
Eduard Daniel Toma <eduard.daniel.toma@adicomsoft.ro>
2021-07-22 16:23:57 +0300

    .

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,7 @@
+import { CoursesService } from './../../services/courses.service';
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import {FormBuilder, Validators} from '@angular/forms';
+import { courseTitleValidator } from '../../validators/course-title.validator';
 
 @Component({
   selector: 'create-course-step-1',
@@ -11,8 +10,30 @@ import {filter} from 'rxjs/operators';
 })
 export class CreateCourseStep1Component implements OnInit {
 
+  form = this.fb.group({
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [courseTitleValidator(this.coursesService)],
+      updateOn: 'blur'
+  }],
+  })
+
+  constructor(private fb: FormBuilder,
+      private coursesService: CoursesService
+    ) {
+
+  }
+
   ngOnInit() {
 
+  }
+  
+  get courseTitle() {
+    return this.form.controls['title'];
   }
 
 }
